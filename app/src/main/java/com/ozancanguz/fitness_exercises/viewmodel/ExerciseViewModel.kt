9cfe9f91ad5ee3e6fee5aa6ba173ec054fb5c7f2
Ozan <ozancangu24@gmail.com>
2023-02-17 01:59:48 +0300

    -Exercise view model created

---
+++ app/src/main/java/com/ozancanguz/fitness_exercises/viewmodel/ExerciseViewModel.kt
@@ -0,0 +1,32 @@
+package com.ozancanguz.fitness_exercises.viewmodel
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.ozancanguz.fitness_exercises.data.Repository
+import com.ozancanguz.fitness_exercises.data.model.Exercise
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ExerciseViewModel@Inject constructor(private val repository: Repository, application: Application):AndroidViewModel(application){
+
+    var exerciseList= MutableLiveData<Exercise>()
+
+
+    fun getExercises(){
+        viewModelScope.launch {
+            val response=repository.remote.getExercise()
+            if(response.isSuccessful){
+                exerciseList.postValue(response.body())
+            }else{
+                Log.d("viewmodel","No data")
+            }
+        }
+    }
+
+
+}
